2bcf4dc4943d6c7201dffd35df5f62d13115fc95
cintiakuhn66 <vitor.reisvc@gmail.com>
2024-09-19 17:04:38 -0300

    Desafio UC1

---
+++ Referências/index.js
@@ -0,0 +1,10 @@
+
+for(let i=10; i>=0;i--){
+    if(i === 0) {
+        console.log('BOOOM')
+    } else if(i%2===0) {
+        console.log('TIC')
+    } else {
+        console.log('TAC')
+    }
+}
